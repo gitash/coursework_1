c6dba36aa0c8a036f94e127d976570453ffd3751
gitash <amacdo217@caledonian.ac.uk>
2019-11-05 15:18:52 +0000

    Code fix: added error for no number provided

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,33 +7,36 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        if (args.length > 0) 
+        if (args.length > 0)
         {
-            try 
+            try
             {
                 Arg1 = Integer.parseInt(args[0]);
-            } 
-            catch (NumberFormatException e) 
+            }
+            catch (NumberFormatException e)
             {
-                System.err.println("Argument " + args[0] + " must be an integer.");
+                System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
             }
         }
+        else
+        {System.err.println("Error: Please enter an integer.");
+	    System.exit(1); }
 
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
+
         while(num != 0)
         {
-	    rem=num%16;
-	    hexadecimal= ch[rem] + hexadecimal;
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
-        
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
+
+        System.out.println("Hexadecimal representation is  : " + hexadecimal);
 
     }
 }
